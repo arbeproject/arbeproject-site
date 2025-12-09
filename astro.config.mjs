ebf568f688bc2ed6c15a98a9cba62b5b415c72b9
caratroll <danieltrabajo@gmail.com>
2025-12-09 15:59:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.arbeproject.org",
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
 
 
 
